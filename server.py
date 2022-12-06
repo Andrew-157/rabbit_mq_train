bbc5bacb3b49a74d04727dc4a9a75da104ea40f5
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-06 12:15:17 +0200

    back to initial server

--- server.py
+++ server.py
@@ -8,8 +8,10 @@ connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 channel.queue_declare(queue='test')
 
-while True:
-    channel.basic_publish(exchange='', routing_key='test',
-                          body='Welcome to RabbitMQ')
 
-    sleep(0.01)
+channel.basic_publish(exchange='', routing_key='test',
+                      body='Welcome to RabbitMQ')
+
+sleep(60)
+
+connection.close()
